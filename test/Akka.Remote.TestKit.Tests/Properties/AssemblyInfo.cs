b2f55bb88d5ee967dccd5ca2209b0465fdfe44db
Seabiscuit <hmkhan@qq.com>
2018-03-13 02:29:25 +0800

    Akka.TestKit.Tests

---
+++ test/Akka.Remote.TestKit.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Akka.NET Project">
+//     Copyright (C) 2009-2018 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2018 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
