6745f5191723039b2810e27d9dac861bac31c682
Seabiscuit <hmkhan@qq.com>
2018-05-20 00:28:04 +0800

    take from akka.net-1.3.7

--- test/Akka.Persistence.Tests/SnapshotFailureRobustnessSpec.cs
+++ test/Akka.Persistence.Tests/SnapshotFailureRobustnessSpec.cs
@@ -69,13 +69,12 @@ namespace Akka.Persistence.Tests
 
             protected override bool ReceiveCommand(object message)
             {
-                if (message is Cmd)
+                if (message is Cmd cmd)
                 {
-                    var cmd = (Cmd) message;
                     Persist(cmd.Payload, _ => SaveSnapshot(cmd.Payload));
                 }
-                else if (message is SaveSnapshotSuccess)
-                    _probe.Tell(((SaveSnapshotSuccess)message).Metadata.SequenceNr);
+                else if (message is SaveSnapshotSuccess success)
+                    _probe.Tell(success.Metadata.SequenceNr);
                 else
                     _probe.Tell(message);
                 return true;
@@ -162,7 +161,7 @@ namespace Akka.Persistence.Tests
                 {
                     var bytes = Encoding.UTF8.GetBytes("b0rk");
                     var tempFile = WithOutputStream(metadata, stream => stream.Write(bytes, 0, bytes.Length));
-					tempFile.MoveTo(GetSnapshotFileForWrite(metadata, "").FullName);
+                    tempFile.MoveTo(GetSnapshotFileForWrite(metadata, "").FullName);
                 }
                 else base.Save(metadata, payload);
             }
