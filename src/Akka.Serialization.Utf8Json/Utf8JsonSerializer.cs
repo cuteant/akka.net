4a660e73030f8025aab94e253b8ee36b25fcc828
cuteant <cuteant@outlook.com>
2019-01-23 13:18:17 +0800

    Akka.Serialization.Utf8Json

---
+++ src/Akka.Serialization.Utf8Json/Utf8JsonSerializer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Configuration;
+using CuteAnt;
+using Utf8Json;
+using Utf8Json.ImmutableCollection;
+
+namespace Akka.Serialization
+{
+    /// <summary>This is a special <see cref="Serializer"/> that serializes and deserializes javascript objects only.
+    /// These objects need to be in the JavaScript Object Notation (JSON) format.</summary>
+    public class Utf8JsonSerializer : Serializer
+    {
+        private readonly IJsonFormatterResolver _resolver;
+        private readonly int _initialBufferSize;
+
+        static Utf8JsonSerializer()
+        {
+            Utf8JsonStandardResolver.TryRegister(ImmutableCollectionResolver.Instance);
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="Utf8JsonSerializer"/> class.</summary>
+        /// <param name="system">The actor system to associate with this serializer. </param>
+        public Utf8JsonSerializer(ExtendedActorSystem system) : this(system, Utf8JsonSerializerSettings.Default) { }
+
+        /// <summary>Initializes a new instance of the <see cref="Utf8JsonSerializer"/> class.</summary>
+        public Utf8JsonSerializer(ExtendedActorSystem system, Config config) : this(system, Utf8JsonSerializerSettings.Create(config)) { }
+
+        /// <summary>Initializes a new instance of the <see cref="Utf8JsonSerializer"/> class.</summary>
+        public Utf8JsonSerializer(ExtendedActorSystem system, Utf8JsonSerializerSettings settings) : base(system)
+        {
+            _initialBufferSize = settings.InitialBufferSize;
+            if (!settings.EnumAsString)
+            {
+                Utf8JsonStandardResolver.TryRegister(Utf8Json.Resolvers.EnumResolver.UnderlyingValue);
+            }
+            var nameMutate = settings.NameMutate.ToLowerInvariant();
+            switch (nameMutate)
+            {
+                case "camelcase":
+                    _resolver = Utf8JsonStandardResolver.CamelCase;
+                    break;
+                case "snakecase":
+                    _resolver = Utf8JsonStandardResolver.SnakeCase;
+                    break;
+                case "original":
+                default:
+                    _resolver = Utf8JsonStandardResolver.Default;
+                    break;
+            }
+        }
+
+        /// <inheritdoc />
+        public override int Identifier => 106;
+
+        /// <inheritdoc />
+        public override bool IncludeManifest => true;
+
+        /// <inheritdoc />
+        public override object DeepCopy(object source)
+        {
+            if (source == null) { return null; }
+
+            var type = source.GetType();
+            var serializedObject = JsonSerializer.SerializeUnsafe(source, _resolver);
+            return JsonSerializer.NonGeneric.Deserialize(type, serializedObject.Array, serializedObject.Offset, _resolver);
+        }
+
+        /// <inheritdoc />
+        public override object FromBinary(byte[] bytes, Type type) => JsonSerializer.NonGeneric.Deserialize(type, bytes, 0, _resolver);
+
+        /// <inheritdoc />
+        public override byte[] ToBinary(object obj)
+        {
+            if (null == obj) { return EmptyArray<byte>.Instance; }
+            return JsonSerializer.Serialize(obj, _resolver);
+        }
+    }
+}
---
+++ src/Akka.Serialization.Utf8Json/Utf8JsonSerializerSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Akka.Configuration;
+
+namespace Akka.Serialization
+{
+    public sealed class Utf8JsonSerializerSettings
+    {
+        public static readonly Utf8JsonSerializerSettings Default = new Utf8JsonSerializerSettings(
+            initialBufferSize: 1024 * 80,
+            nameMutate: "original",
+            enumAsString: true);
+
+        public static Utf8JsonSerializerSettings Create(Config config)
+        {
+            if (config == null) throw new ArgumentNullException(nameof(config), "MsgPackSerializerSettings require a config, default path: `akka.serializers.msgpack`");
+
+            return new Utf8JsonSerializerSettings(
+                initialBufferSize: config.GetInt("initial-buffer-size", 1024 * 80),
+                nameMutate: config.GetString("name-mutate", "original"),
+                enumAsString: config.GetBoolean("enum-as-string", true));
+        }
+
+        public Utf8JsonSerializerSettings(int initialBufferSize, string nameMutate, bool enumAsString)
+        {
+            if (initialBufferSize < 1024) { initialBufferSize = 1024; }
+            if (initialBufferSize > 81920) { initialBufferSize = 81920; }
+            InitialBufferSize = initialBufferSize;
+
+            NameMutate = nameMutate;
+            EnumAsString = enumAsString;
+        }
+
+
+        /// <summary>The initial buffer size.</summary>
+        public readonly int InitialBufferSize;
+
+        public string NameMutate { get; }
+
+        public bool EnumAsString { get; }
+    }
+}
