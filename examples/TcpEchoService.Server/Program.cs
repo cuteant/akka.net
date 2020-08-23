5050841a2d8cd0d019a7bdb4b9d3d5fc21e9268d
cuteant <cuteant@outlook.com>
2020-08-24 00:30:41 +0800

    Update examples

--- examples/TcpEchoService.Server/Program.cs
+++ examples/TcpEchoService.Server/Program.cs
@@ -7,20 +7,21 @@
 
 using System;
 using System.Net;
+using System.Threading.Tasks;
 using Akka.Actor;
 
 namespace TcpEchoService.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using (var system = ActorSystem.Create("echo-server-system"))
             {
                 var port = 9001;
                 var actor = system.ActorOf(Props.Create(() => new EchoService(new IPEndPoint(IPAddress.Any, port))), "echo-service");
 
-                /**
+                /*
                  *  Now you should be able to connect with current TCP actor using i.e. telnet command:
                  *  $> telnet 127.0.0.1 9001
                  */
@@ -28,6 +29,9 @@ namespace TcpEchoService.Server
                 Console.WriteLine("TCP server is listening on *:{0}", port);
                 Console.WriteLine("ENTER to exit...");
                 Console.ReadLine();
+
+                // Close connection to avoid error message in console
+                await actor.Ask(new EchoService.StopServer());
             }
         }
     }
